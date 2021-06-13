4ed3f762c320f120f5dbadfd8014518e48e6ef52
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-06-13 16:30:02 -0300

    formulario criado

---
+++ script.js
@@ -0,0 +1,15 @@
+function states() {
+  let sigla = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
+  // let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+
+  let cidades = document.querySelector('#estado');
+
+  for (let i = 0; i < sigla.length; i += 1) {
+    const cidade = document.createElement('option');
+    cidades.appendChild(cidade);
+    cidade.innerHTML = `<option name ='${sigla[i]}' value = '${sigla[i]}'> ${sigla[i]}</option>`;
+  }
+}
+
+states();
